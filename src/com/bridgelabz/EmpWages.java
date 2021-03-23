c3b4db35d3541d56f43f38155a20db929bd16432
amarprajapati99 <amarprajapati99@gmail.com>
2021-03-24 01:22:11 +0530

    CalWageTillNoOfWorkingDays

--- src/com/bridgelabz/EmpWages.java
+++ src/com/bridgelabz/EmpWages.java
@@ -1,16 +1,19 @@
 package com.bridgelabz;
 
-public class EmpWages{
+public class EmpWages {
 	public static final int Is_Part_Time = 1;
 	public static final int Is_Full_Time = 2;
 	public static final int Emp_Rate_Per_Hrs = 20;
 	public static final int No_Of_Working_Days = 2;
+	public static final int Max_Hr_In_Month = 10;
 
 	public static void main(String[] args) {
 		int EmpWage = 0;
 		int Emphrs = 0;
-		int totalEmpWage = 0;
-		for (int days = 0; days < No_Of_Working_Days; days++) {
+		// int totalEmpWage = 0;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
+		while (totalEmpHrs <= Max_Hr_In_Month && totalWorkingDays <= No_Of_Working_Days) {
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
 			case Is_Part_Time:
@@ -23,9 +26,14 @@ public class EmpWages{
 				Emphrs = 0;
 			}
 			EmpWage = Emphrs * Emp_Rate_Per_Hrs;
-			totalEmpWage += EmpWage;
+			// totalEmpWage += EmpWage;
+			totalEmpHrs += Emphrs;
 			System.out.println("Emp Wage" + " " + EmpWage);
+			System.out.println("Day : " + " " + totalWorkingDays + "Emphrs: " + Emphrs);
+
 		}
+		int totalEmpWage = totalEmpHrs * Emp_Rate_Per_Hrs;
+		System.out.println("total Emp Wage" + " " + totalEmpWage);
 		System.out.println("totalEmpWage" + " " + totalEmpWage);
 
 	}
