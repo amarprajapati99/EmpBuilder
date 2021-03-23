5c6ff788c5fcba4a5ab6e0beed92c1d9c74e9e43
amarprajapati99 <amarprajapati99@gmail.com>
2021-03-24 00:58:13 +0530

    using case statement

--- src/com/bridgelabz/EmpWages.java
+++ src/com/bridgelabz/EmpWages.java
@@ -1,27 +1,25 @@
 package com.bridgelabz;
 
 public class EmpWages {
+	public static final int Is_Part_Time=1;
+	public static final int Is_Full_Time=2;
+	public static final int Emp_Rate_Per_Hrs=20;
 	public static void main(String[] args) {
-		System.out.println("Welcome employee builder");
-		int Is_Part_Time = 1;
-		int IS_FULL_TIME = 2;
-		final int emp_per_rate = 20;
-		int empHr = 0;
-		int empWage = 0;
-		int emphrs;
-		// find random number
-		double empcheck = Math.floor(Math.random() * 10) % 3;
-		if (empcheck == IS_FULL_TIME) {
-			System.out.println("Employee is present");
-			empHr = 8;
-		} else if (empcheck == Is_Part_Time) {
-			System.out.println("Employee is absent");
-			empHr = 4;
-		} else
-			// calculate Employee Wage
-			emphrs = 0;
-		empWage = empHr * emp_per_rate;
-		System.out.println("EmpWage :" + " " + empWage);
-	}
+		int EmpWage=0;
+		int Emphrs=0;
+		int empCheck=(int)Math.floor(Math.random() *10) %3 ;
+		switch (empCheck) {
+		case Is_Part_Time:
+			Emphrs=4;
+			break;
+		case Is_Full_Time:
+			Emphrs=8;
+			break;
+		default:
+			Emphrs=0;
+		}
+		EmpWage=Emphrs*Emp_Rate_Per_Hrs;
+		System.out.println("Emp Wage"+ " "+ EmpWage);
 
+	}
 }
