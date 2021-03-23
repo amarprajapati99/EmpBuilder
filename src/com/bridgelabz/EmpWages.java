547ceef2619d02ee31973856fc9358d9c961a1ff
amarprajapati99 <amarprajapati99@gmail.com>
2021-03-24 00:21:07 +0530

    Employee is present or absent

--- src/com/bridgelabz/EmpWages.java
+++ src/com/bridgelabz/EmpWages.java
@@ -3,6 +3,13 @@ package com.bridgelabz;
 public class EmpWages {
 	public static void main(String[] args) {
 		System.out.println("Welcome employee builder");
+		int IS_FULL_TIME = 1;
+		double empcheck = Math.floor(Math.random() * 10) % 2;
+		if (empcheck == IS_FULL_TIME) {
+			System.out.println("Employee is present");
+		} else {
+			System.out.println("Employee is absent");
+		}
 	}
 
 }
