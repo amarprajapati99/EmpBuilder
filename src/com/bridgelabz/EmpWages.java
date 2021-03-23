1b49edb74e647783f75588c8066e3b1fbce84890
amarprajapati99 <amarprajapati99@gmail.com>
2021-03-24 00:51:13 +0530

    Employee is working part time or full time

--- src/com/bridgelabz/EmpWages.java
+++ src/com/bridgelabz/EmpWages.java
@@ -3,20 +3,23 @@ package com.bridgelabz;
 public class EmpWages {
 	public static void main(String[] args) {
 		System.out.println("Welcome employee builder");
-		int IS_FULL_TIME = 1;
+		int Is_Part_Time = 1;
+		int IS_FULL_TIME = 2;
 		final int emp_per_rate = 20;
 		int empHr = 0;
 		int empWage = 0;
-		// find random  number
-		double empcheck = Math.floor(Math.random() * 10) % 2;
+		int emphrs;
+		// find random number
+		double empcheck = Math.floor(Math.random() * 10) % 3;
 		if (empcheck == IS_FULL_TIME) {
 			System.out.println("Employee is present");
 			empHr = 8;
-		} else {
+		} else if (empcheck == Is_Part_Time) {
 			System.out.println("Employee is absent");
-			empHr = 0;
-		}
-		// calculate Employee Wage
+			empHr = 4;
+		} else
+			// calculate Employee Wage
+			emphrs = 0;
 		empWage = empHr * emp_per_rate;
 		System.out.println("EmpWage :" + " " + empWage);
 	}
