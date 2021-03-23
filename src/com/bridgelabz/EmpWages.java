2e0b4d9669769423198953ccf69b4f11bbd8993f
amarprajapati99 <amarprajapati99@gmail.com>
2021-03-24 00:32:43 +0530

    Employee part time wage

--- src/com/bridgelabz/EmpWages.java
+++ src/com/bridgelabz/EmpWages.java
@@ -4,12 +4,21 @@ public class EmpWages {
 	public static void main(String[] args) {
 		System.out.println("Welcome employee builder");
 		int IS_FULL_TIME = 1;
+		final int emp_per_rate = 20;
+		int empHr = 0;
+		int empWage = 0;
+		// find random  number
 		double empcheck = Math.floor(Math.random() * 10) % 2;
 		if (empcheck == IS_FULL_TIME) {
 			System.out.println("Employee is present");
+			empHr = 8;
 		} else {
 			System.out.println("Employee is absent");
+			empHr = 0;
 		}
+		// calculate Employee Wage
+		empWage = empHr * emp_per_rate;
+		System.out.println("EmpWage :" + " " + empWage);
 	}
 
 }
